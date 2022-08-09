1edc06aa59e49f39a996fe78ec7f6ded3e1972b2
anthony-alepra <Anthony.alepra@gmail.com>
2022-08-09 23:33:06 +0000

    finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
+require "net/http"
+require "json"
+
 class ApplicationController < ActionController::Base
+  def home
+    render({ :template => "home.html.erb" })
+  end
+
+  def forex
+    url = "https://api.exchangerate.host/symbols"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response_obj = JSON.parse(response)
+    @currency = response_obj["symbols"].keys
+
+    render({ :template => "forex.html.erb" })
+  end
+
+  def convert_from
+    @symbol1 = params.fetch("symbol")
+    url = "https://api.exchangerate.host/symbols"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response_obj = JSON.parse(response)
+    @currency = response_obj["symbols"].keys
+
+    render({ :template => "forex_convert_from.html.erb" })
+  end
+
+  def convert_from_to
+    @symbol1 = params.fetch("symbol1")
+    @symbol2 = params.fetch("symbol2")
+    url = "https://api.exchangerate.host/convert?from=" + @symbol1 + "&to=" + @symbol2
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response_obj = JSON.parse(response)
+
+    @exchangerate = response_obj["result"]
+
+    render({ :template => "converted.html.erb" })
+  end
+
+  def covid
+    render({ :template => "covid.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "application", :action => "home" })
+  get("/forex", { :controller => "application", :action => "forex" })
+  get("/covid", { :controller => "application", :action => "covid" })
+  get("/forex/:symbol", { :controller => "application", :action => "convert_from" })
+  get("/forex/:symbol1/:symbol2", { :controller => "application", :action => "convert_from_to" })
 end
